e194943f7f0b91938a10fb3dd0d4f7312780b26a
cruvex <cruvexx@gmail.com>
2025-04-03 00:59:22 +0200

    WIP: use oauth2 for discord authentication

--- src/service/discord/discord_auth.rs
+++ src/service/discord/discord_auth.rs
@@ -1,20 +1,34 @@
 use crate::config::{DiscordConfig, RedisConfig};
 use crate::model::discord::{DiscordAuthorizationCodeRequest, DiscordRefreshTokenRequest, DiscordToken, User};
 use crate::web::error::Error;
-use redis::{Commands, ExpireOption};
+use oauth2::basic::{BasicClient, BasicErrorResponse, BasicRevocationErrorResponse, BasicTokenIntrospectionResponse, BasicTokenResponse};
+use oauth2::url::Url;
+use oauth2::{AuthUrl, ClientId, ClientSecret, CsrfToken, EndpointNotSet, EndpointSet, RedirectUrl, Scope, StandardRevocableToken, TokenUrl};
+use redis::{Commands, ExpireOption, ToRedisArgs};
 use redis_pool::{RedisPool, SingleRedisPool};
 use reqwest::Client;
 use tracing::debug;
 use urlencoding::encode;
 
+pub type DiscordAuthClient = BasicClient;
+
 #[derive(Clone)]
 pub struct DiscordAuthService {
-    client: Client,
-    api_version: String,
-    client_id: String,
-    client_secret: String,
-    redirect_url: String,
-    scopes: String,
+    http_client: Client,
+    // Not really happy with this but what do I know
+    pub oauth_client: oauth2::Client<
+        BasicErrorResponse,
+        BasicTokenResponse,
+        BasicTokenIntrospectionResponse,
+        StandardRevocableToken,
+        BasicRevocationErrorResponse,
+        EndpointSet,
+        EndpointNotSet,
+        EndpointNotSet,
+        EndpointNotSet,
+        EndpointSet,
+    >,
+    pub scopes: String,
     redis: SingleRedisPool,
 }
 
@@ -23,112 +37,121 @@ impl DiscordAuthService {
         let redis = redis::Client::open(redis_config.url.to_string()).expect("Failed to create Redis client");
         let redis_pool = RedisPool::from(redis);
 
+        let discord_oauth_url = "https://discord.com/oauth2/authorize".to_string();
+        let discord_token_url = format!("https://discord.com/api/v{}/oauth2/token", &discord_config.api_version);
+
+        let http_client = reqwest::ClientBuilder::new()
+            // Following redirects opens the client up to SSRF vulnerabilities.
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .expect("Client should build");
+
+        let oauth_client = DiscordAuthClient::new(ClientId::new(discord_config.client_id.to_string()))
+            .set_client_secret(ClientSecret::new(discord_config.client_secret.to_string()))
+            .set_auth_uri(AuthUrl::new(discord_oauth_url).expect("Failed to parse Discord OAuth URL"))
+            .set_token_uri(TokenUrl::new(discord_token_url).expect("Failed to parse Discord token URL"))
+            .set_redirect_uri(RedirectUrl::new(discord_config.redirect_url.to_string()).expect("Failed to parse Discord redirect URL"));
+
         Self {
-            client: Client::new(),
-            api_version: discord_config.api_version.clone(),
-            client_id: discord_config.client_id.clone(),
-            client_secret: discord_config.client_secret.clone(),
-            redirect_url: discord_config.redirect_url.clone(),
-            scopes: discord_config.scopes.clone(),
+            http_client,
+            oauth_client,
+            scopes: discord_config.scopes.to_string(),
             redis: redis_pool,
         }
     }
 
-    pub fn get_oauth_url(&self) -> String {
-        self.oauth_url_for(&format!(
-            "authorize?client_id={}&redirect_uri={}&response_type=code&scope={}",
-            &self.client_id,
-            encode(&self.redirect_url),
-            encode(&self.scopes)
-        ))
+    pub fn init_auth(&self) -> (Url, CsrfToken) {
+        self.oauth_client.authorize_url(CsrfToken::new_random).add_scope(Scope::new(self.scopes.clone())).url()
     }
 
-    pub async fn authenticate(&self, code: &str) -> Result<String, Error> {
-        let token = self.get_discord_token_by_code(code).await;
-        let user = self.get_discord_self_user(&token.access_token).await?;
-
-        self.store_token(&user.id, &token).await?;
-
-        Ok(user.id)
-    }
-
-    /// Exchanges the authorization code for an access token.
-    pub async fn get_discord_token_by_code(&self, code: &str) -> DiscordToken {
-        self.client
-            .post(&self.api_url_for("oauth2/token"))
-            .form(&DiscordAuthorizationCodeRequest {
-                grant_type: "authorization_code".to_string(),
-                code: code.to_string(),
-                redirect_uri: self.redirect_url.to_string(),
-            })
-            .basic_auth(&self.client_id, Some(&self.client_secret))
-            .send()
-            .await
-            .expect("Failed to send token request")
-            .json::<DiscordToken>()
-            .await
-            .expect("Failed to deserialize token response")
-    }
-
-    /// Exchanges the refresh token for an access token.
-    pub async fn get_discord_token_by_refresh_token(&self, refresh_token: &str) -> DiscordToken {
-        self.client
-            .post(&self.api_url_for("oauth2/token"))
-            .form(&DiscordRefreshTokenRequest {
-                grant_type: "refresh_token".to_string(),
-                refresh_token: refresh_token.to_string(),
-                redirect_uri: self.redirect_url.to_string(),
-            })
-            .basic_auth(&self.client_id, Some(&self.client_secret))
-            .send()
-            .await
-            .expect("Failed to send token request")
-            .json::<DiscordToken>()
-            .await
-            .expect("Failed to deserialize token response")
-    }
-
-    /// Calls Discord’s `/users/@me` endpoint with the given access token to fetch user info.
-    pub async fn get_discord_self_user(&self, access_token: &str) -> Result<User, Error> {
-        let user = self
-            .client
-            .get(&self.api_url_for("users/@me"))
-            .bearer_auth(access_token)
-            .send()
-            .await
-            .map_err(|e| Error::DiscordApiError(e.to_string()))?
-            .json::<User>()
-            .await
-            .map_err(|e| Error::DiscordApiError(e.to_string()))?;
-
-        Ok(user)
-    }
-
-    fn api_url_for(&self, path: &str) -> String {
-        format!("https://discord.com/api/v{}/{}", self.api_version, path)
-    }
-
-    fn oauth_url_for(&self, path: &str) -> String {
-        format!("https://discord.com/oauth2/{}", path)
-    }
-
-    async fn store_token(&self, user_id: &str, token: &DiscordToken) -> Result<(), Error> {
-        let mut conn = self.redis.get_connection().map_err(|_e| Error::RedisConnectionError)?;
-        let user_key = format!("user:{}", user_id);
-        let redis_operations = [("access_token", &token.access_token), ("refresh_token", &token.refresh_token)];
-
-        debug!("Storing token information in Redis");
-        let _: () = conn.hset_multiple(&user_key, &redis_operations).map_err(|e| {
-            debug!("Failed to store tokens: {:?}", e);
-            Error::RedisOperationError(e.to_string())
-        })?;
-
-        debug!("Setting token expiration");
-        let _: () = conn.hexpire(&user_key, token.expires_in - 5, ExpireOption::NONE, "access_token").map_err(|e| {
-            debug!("Failed to set expiration: {:?}", e);
-            Error::RedisOperationError(e.to_string())
-        })?;
-
-        Ok(())
-    }
+    // pub async fn authenticate(&self, code: &str) -> Result<String, Error> {
+    //     let (auth_url, csrf_token) = client.authorize_url(CsrfToken::new_random).add_scope(Scope::new("identify".to_string())).url();
+    //
+    //     let token = self.get_discord_token_by_code(code).await;
+    //     let user = self.get_discord_self_user(&token.access_token).await?;
+    //
+    //     self.store_token(&user.id, &token).await?;
+    //
+    //     Ok(user.id)
+    // }
+    //
+    // /// Exchanges the authorization code for an access token.
+    // pub async fn get_discord_token_by_code(&self, code: &str) -> DiscordToken {
+    //     self.client
+    //         .post(&self.api_url_for("oauth2/token"))
+    //         .form(&DiscordAuthorizationCodeRequest {
+    //             grant_type: "authorization_code".to_string(),
+    //             code: code.to_string(),
+    //             redirect_uri: self.redirect_url.to_string(),
+    //         })
+    //         .basic_auth(&self.client_id, Some(&self.client_secret))
+    //         .send()
+    //         .await
+    //         .expect("Failed to send token request")
+    //         .json::<DiscordToken>()
+    //         .await
+    //         .expect("Failed to deserialize token response")
+    // }
+    //
+    // /// Exchanges the refresh token for an access token.
+    // pub async fn get_discord_token_by_refresh_token(&self, refresh_token: &str) -> DiscordToken {
+    //     self.client
+    //         .post(&self.api_url_for("oauth2/token"))
+    //         .form(&DiscordRefreshTokenRequest {
+    //             grant_type: "refresh_token".to_string(),
+    //             refresh_token: refresh_token.to_string(),
+    //             redirect_uri: self.redirect_url.to_string(),
+    //         })
+    //         .basic_auth(&self.client_id, Some(&self.client_secret))
+    //         .send()
+    //         .await
+    //         .expect("Failed to send token request")
+    //         .json::<DiscordToken>()
+    //         .await
+    //         .expect("Failed to deserialize token response")
+    // }
+    //
+    // /// Calls Discord’s `/users/@me` endpoint with the given access token to fetch user info.
+    // pub async fn get_discord_self_user(&self, access_token: &str) -> Result<User, Error> {
+    //     let user = self
+    //         .client
+    //         .get(&self.api_url_for("users/@me"))
+    //         .bearer_auth(access_token)
+    //         .send()
+    //         .await
+    //         .map_err(|e| Error::DiscordApiError(e.to_string()))?
+    //         .json::<User>()
+    //         .await
+    //         .map_err(|e| Error::DiscordApiError(e.to_string()))?;
+    //
+    //     Ok(user)
+    // }
+    //
+    // fn api_url_for(&self, path: &str) -> String {
+    //     format!("https://discord.com/api/v{}/{}", self.api_version, path)
+    // }
+    //
+    // fn oauth_url_for(&self, path: &str) -> String {
+    //     format!("https://discord.com/oauth2/{}", path)
+    // }
+    //
+    // async fn store_token(&self, user_id: &str, token: &DiscordToken) -> Result<(), Error> {
+    //     let mut conn = self.redis.get_connection().map_err(|_e| Error::RedisConnectionError)?;
+    //     let user_key = format!("user:{}", user_id);
+    //     let redis_operations = [("access_token", &token.access_token), ("refresh_token", &token.refresh_token)];
+    //
+    //     debug!("Storing token information in Redis");
+    //     let _: () = conn.hset_multiple(&user_key, &redis_operations).map_err(|e| {
+    //         debug!("Failed to store tokens: {:?}", e);
+    //         Error::RedisOperationError(e.to_string())
+    //     })?;
+    //
+    //     debug!("Setting token expiration");
+    //     let _: () = conn.hexpire(&user_key, token.expires_in - 5, ExpireOption::NONE, "access_token").map_err(|e| {
+    //         debug!("Failed to set expiration: {:?}", e);
+    //         Error::RedisOperationError(e.to_string())
+    //     })?;
+    //
+    //     Ok(())
+    // }
 }
--- src/web/routes_auth_discord.rs
+++ src/web/routes_auth_discord.rs
@@ -2,6 +2,7 @@ use axum::extract::{Query, State};
 use axum::response::Html;
 use axum::routing::get;
 use axum::{Json, Router};
+use oauth2::{CsrfToken, Scope};
 use serde::Deserialize;
 use serde_json::{Value, json};
 use serde_with::NoneAsEmptyString;
@@ -18,13 +19,15 @@ use crate::web::error::Error;
 use crate::web::{ACCESS_TOKEN_COOKIE, REFRESH_TOKEN_COOKIE};
 
 pub fn routes(state: AppState) -> Router {
-    Router::new().route("/auth/discord/oauth-url", get(oauth_url)).route("/auth/discord/callback", get(callback)).with_state(state)
+    Router::new().route("/auth/discord", get(auth_discord)).route("/auth/discord/callback", get(auth_discord_callback)).with_state(state)
 }
 
-pub async fn oauth_url(State(state): State<AppState>) -> Result<Json<Value>> {
-    debug!("{:<12} - {}", "HANDLER", "auth_discord_oauth_url");
+pub async fn auth_discord(State(state): State<AppState>) -> Result<Json<Value>> {
+    debug!("{:<12} - {}", "HANDLER", "auth_discord");
+    let (auth_url, csrf_token) = state.discord.auth.init_auth();
+
     Ok(Json(json!({
-        "url": state.discord.auth.get_oauth_url()
+        "url": auth_url
     })))
 }
 
@@ -35,7 +38,11 @@ pub struct DiscordCallbackQueryParams {
     code: Option<String>,
 }
 
-pub async fn callback(cookies: Cookies, State(state): State<AppState>, Query(params): Query<DiscordCallbackQueryParams>) -> Result<Html<String>> {
+pub async fn auth_discord_callback(
+    cookies: Cookies,
+    State(state): State<AppState>,
+    Query(params): Query<DiscordCallbackQueryParams>,
+) -> Result<Html<String>> {
     debug!("{:<12} - {}", "HANDLER", "auth_discord_callback");
 
     let code = params.code.as_ref().ok_or_else(|| {
@@ -43,29 +50,29 @@ pub async fn callback(cookies: Cookies, State(state): State<AppState>, Query(par
         Error::NoDiscordCodeInPath
     })?;
 
-    let user_id = state.discord.auth.authenticate(code).await?;
-
-    let mut claims = Claims::new(&user_id);
-
-    let access_token = state.jwt.generate_access_token(&mut claims).map_err(|_e| Error::JwtTokenGenerationError)?;
-    let mut access_token_cookie = Cookie::new(ACCESS_TOKEN_COOKIE, access_token);
-    access_token_cookie.set_http_only(true);
-    access_token_cookie.set_path("/");
-    access_token_cookie.set_same_site(SameSite::Strict);
-    access_token_cookie.set_secure(state.jwt.secure_cookie);
-
-    let refresh_token = state.jwt.generate_refresh_token(&mut claims).map_err(|_e| Error::JwtTokenGenerationError)?;
-    let mut refresh_token_cookie = Cookie::new(REFRESH_TOKEN_COOKIE, refresh_token);
-    refresh_token_cookie.set_http_only(true);
-    refresh_token_cookie.set_path("/auth/refresh");
-    refresh_token_cookie.set_same_site(SameSite::Strict);
-    refresh_token_cookie.set_secure(state.jwt.secure_cookie);
-
-    cookies.add(access_token_cookie);
-    cookies.add(refresh_token_cookie);
-
-    // TODO: Properly implement response html with error state
-    // Return the appropriate HTML response
+    // let user_id = state.discord.auth.authenticate(code).await?;
+    //
+    // let mut claims = Claims::new(&user_id);
+    //
+    // let access_token = state.jwt.generate_access_token(&mut claims).map_err(|_e| Error::JwtTokenGenerationError)?;
+    // let mut access_token_cookie = Cookie::new(ACCESS_TOKEN_COOKIE, access_token);
+    // access_token_cookie.set_http_only(true);
+    // access_token_cookie.set_path("/");
+    // access_token_cookie.set_same_site(SameSite::Strict);
+    // access_token_cookie.set_secure(state.jwt.secure_cookie);
+    //
+    // let refresh_token = state.jwt.generate_refresh_token(&mut claims).map_err(|_e| Error::JwtTokenGenerationError)?;
+    // let mut refresh_token_cookie = Cookie::new(REFRESH_TOKEN_COOKIE, refresh_token);
+    // refresh_token_cookie.set_http_only(true);
+    // refresh_token_cookie.set_path("/auth/refresh");
+    // refresh_token_cookie.set_same_site(SameSite::Strict);
+    // refresh_token_cookie.set_secure(state.jwt.secure_cookie);
+    //
+    // cookies.add(access_token_cookie);
+    // cookies.add(refresh_token_cookie);
+    //
+    // // TODO: Properly implement response html with error state
+    // // Return the appropriate HTML response
     render_callback_response().await
 }
 
