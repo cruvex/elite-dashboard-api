c212a83bab5ec4477398f23bfdee402f091ffaff
cruvex <cruvexx@gmail.com>
2025-04-30 14:24:19 +0200

    bugfix: also refresh cookie ttl when refreshing session

--- src/web/middleware/mw_session.rs
+++ src/web/middleware/mw_session.rs
@@ -11,7 +11,7 @@ use tracing::{debug, trace};
 
 use crate::web::error::Error;
 
-use crate::app::constants::SESSION_COOKIE_NAME;
+use crate::app::constants::{ONE_MONTH, SESSION_COOKIE_NAME};
 use crate::app::error::AppError;
 use crate::model::session::Session;
 use crate::service::SessionService;
@@ -31,10 +31,13 @@ pub async fn mw_session_require(
 
     debug!("{:<12} - Valid session", "MIDDLEWARE");
 
-    // If requesting current session user refresh session ttl
+    // If requesting current session user refresh session and cookie ttl
     if req.uri().path() == "/elites/@me" {
         debug!("{:<12} - Refreshing session", "MIDDLEWARE");
-        session_store.refresh_session_ttl(&session_id).await?
+        session_store.refresh_session_ttl(&session_id).await?;
+
+        let new_cookie = session_store.create_session_cookie(session_id, ONE_MONTH);
+        cookies.add(new_cookie);
     }
 
     req.extensions_mut().insert(session.clone());
