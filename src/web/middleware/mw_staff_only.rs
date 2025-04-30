0796a984afdbfe3b5d05f17c3a8427d6a97391a0
cruvex <cruvexx@gmail.com>
2025-04-30 17:55:54 +0200

    small cleanup

--- src/web/middleware/mw_staff_only.rs
+++ src/web/middleware/mw_staff_only.rs
@@ -1,15 +1,14 @@
 use crate::app::error::AppError;
 use crate::model::session::Session;
-use crate::web::error::Error::{SessionNotFound, StaffOnly};
+use crate::web::error::Error::StaffOnly;
 use axum::body::Body;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
 use tracing::trace;
 
-pub async fn mw_staff_only(req: Request<Body>, next: Next) -> Result<Response, AppError> {
+pub async fn mw_staff_only(session: Session, req: Request<Body>, next: Next) -> Result<Response, AppError> {
     trace!("{:<12} - mw_staff_only", "MIDDLEWARE");
-    let session = req.extensions().get::<Session>().ok_or(SessionNotFound)?.clone();
 
     if !session.user.is_staff() {
         return Err(StaffOnly.into());
